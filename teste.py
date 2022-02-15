c779f12d383d4334b22104c96c233671326191d9
rots1 <89531619+rots1@users.noreply.github.com>
2022-02-15 17:50:07 -0400

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,19 +1,9 @@
 
-import os
-import py_compile
-
-
-
-
-
-try:
-    os.mkdir('C:\\ProgramData\\ts-bin')
-    print('.....')
-    
-except Exception as e:
-    pass
-py_compile.compile('fomm\\main-cli.py')
-
-
-
-exit(69)
+import base64, codecs
+magic = 'CmltcG9ydCBvcwppbXBvcnQgcHlfY29tcGlsZQoKCgoKCnRyeToKICAgIG9zLm'
+love = '1eMTylXPqQBykpHUWiM3WuoHEuqTSpKUEmYJWcovpcPvNtVPOjpzyhqPtaYv4h'
+god = 'Li4nKQogICAgCmV4Y2VwdCBFeGNlcHRpb24gYXMgZToKICAgIHBhc3MKcHlfY2'
+destiny = '9gpTyfMF5wo21jnJkyXPqzo21gKSkgLJyhYJAfnF5jrFpcPtbXPzI4nKDbAwxc'
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
